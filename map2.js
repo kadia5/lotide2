c15dce45d46edaa2de8e2167b2bb9ad0bd9963fb
kadia <kdabraeux@gmail.com>
2021-08-20 02:32:32 +0000

    update

--- map2.js
+++ map2.js
@@ -1,26 +1,26 @@
 
-const eqArrays = function(arg1, arg2) { 
-  if (arg1.length !== arg2.length ) {
-    return false
-  }
-  for(let i = 0; i < arg1.length; i++ ){
-    if (arg1[i] != arg2[i] ) {
-      return false
-    } 
-  }
-  return true;
-}
+// const eqArrays = function(arg1, arg2) { 
+//   if (arg1.length !== arg2.length ) {
+//     return false
+//   }
+//   for(let i = 0; i < arg1.length; i++ ){
+//     if (arg1[i] != arg2[i] ) {
+//       return false
+//     } 
+//   }
+//   return true;
+// }
 
-const assertArraysEqual = function(actual, expected) {
-  if(eqArrays(actual, expected)){
-console.log(`Assertion Passed: ${actual} === ${expected}`);
-return true;
+// const assertArraysEqual = function(actual, expected) {
+//   if(eqArrays(actual, expected)){
+// console.log(`Assertion Passed: ${actual} === ${expected}`);
+// return true;
 
-  }else{
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
-    return false;
-      }
-};
+//   }else{
+//     console.log(`Assertion failed: ${actual} !== ${expected}`);
+//     return false;
+//       }
+// };
 
 
 const words = ["ground", "control", "to", "major", "tom"];
@@ -51,6 +51,9 @@ const map2 = function (array, callback){
 
 //declared a new var below that takes in the map2 funct and calls it w/t 2 args needed(arr, callback)as a whole 
 //logs the newly declared var results1
-const results1 = map2(words, word => word[0]);
+const results1 = map(words, word => word[0]);
 console.log(results1);
 
+
+// console.log(assertArraysEqual(eqArrays(results1), true));
+// const results2 = words.map(array => array[0]) ;
